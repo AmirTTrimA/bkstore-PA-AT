b12a505ea083c5f28df572a29ba5ce88447eceb2
AmirttrimA <amirsrst@gmail.com>
2026-08-19 20:40:29 +0330

    chore(wallet): register wallet models in admin

--- backend/wallet/admin.py
+++ backend/wallet/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.transaction import WalletTransaction
+from .models.wallet import Wallet
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "balance",
+        "currency",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
+    list_filter = (
+        "currency",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(WalletTransaction)
+class WalletTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "wallet",
+        "transaction_type",
+        "amount",
+        "balance_after",
+        "created_at",
+    )
+    search_fields = (
+        "wallet__user__username",
+        "wallet__user__email",
+        "description",
+    )
+    list_filter = (
+        "transaction_type",
+        "created_at",
+    )
+    readonly_fields = (
+        "created_at",
+    )
